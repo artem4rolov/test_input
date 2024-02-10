a0e0b6d33967e093f34521df3008be5ec45e7385
Artem Frolov <frolov@mad-soft.ru>
2024-02-10 23:51:19 +0300

    test 5

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
         <form onSubmit={() => {}}>
           <input
             size={'large'}
-            pattern="\\s\(\d{3}\)\s\d{3,}-\d{2}-\d{2}"
+            pattern="\+7\s\(\d{3}\)\s\d{3,}-\d{2}-\d{2}"
             name="phone"
             type="tel"
             inputMode="tel"
